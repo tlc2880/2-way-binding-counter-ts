1f98a1f213a9f896036b6313c2dd36b88ef0abc9
tlc2880 <tommylcao@gmail.com>
2024-01-27 12:06:03 -0800

    Add push object

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,24 @@ function App() {
   const [value, setValue] = useState(0);
   const [multVal, setMultVal] = useState(1);
   const [arr, setArr] = useState<number[]>([1, 2]);
-  const [strArr, setStrArr] = useState(["One", "Two"]);
+  const [strArr, setStrArr] = useState<string[]>(["One", "Two"]);
   const [obj, setObj] = useState({
     objNum: 0,
     objNumArr: [11, 22],
     objStrArr: ["Five", "Six"]
   });
+  const [arrObj, setArrObj] = useState([
+    {
+      str1: "One",
+      str2: "Two",
+      str3: "Three"
+    }
+  ]);
+
   const incHandler = (value: number) => {
     setValue(value);
   };
+  
   const multArr = () => {
     let numArrTemp = [...arr];
     numArrTemp.forEach((item, index, arr) => {
@@ -30,12 +39,23 @@ function App() {
     });
     obj.objNumArr = numArrTemp;
   };
+
+  const pushObj = () => {
+    let newObj = {
+      str1: "Four",
+      str2: "Five",
+      str3: "Six"
+    };
+    setArrObj([...arrObj, newObj]);
+  };
+
   return (
     <div className="App">
       <h4 style={{ color: "red" }}>Counter: {value}</h4>
       <h4 style={{ color: "blue" }}>Number Array: {JSON.stringify(arr)}</h4>
       <h4 style={{ color: "green" }}>String Array: {JSON.stringify(strArr)}</h4>
       <h4 style={{ color: "purple" }}>String Array: {JSON.stringify(obj)}</h4>
+      <h4 style={{ color: "black" }}>String Array: {JSON.stringify(arrObj)}</h4>
       <Counter
         value={value}
         incHandler={incHandler}
@@ -52,6 +72,7 @@ function App() {
         onChange={(e) => setMultVal(parseInt(e.target.value, 10))}
       />
       <button onClick={() => multArr()}>Mult array</button>
+      <button onClick={() => pushObj()}>Push obj</button>
     </div>
   );
 }
